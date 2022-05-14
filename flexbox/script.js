0d95659c164bb1086f077a1221b32de21f1c3f57
Anand <as08kvs@gmail.com>
2022-05-14 14:30:04 +0530

    add fluxbox folder

---
+++ flexbox/script.js
@@ -0,0 +1,18 @@
+function blue() {
+    document.getElementById("body").setAttribute("class", "body-blue"); 
+    document.getElementById("navbar").setAttribute("class", "navbar navbar-blue");
+    document.getElementById("header").setAttribute("class", "header header-blue"); 
+    document.getElementById("boxes").setAttribute("class", "boxes boxes-blue"); 
+}
+function red() {
+    document.getElementById("body").setAttribute("class", "body-red"); 
+    document.getElementById("navbar").setAttribute("class", "navbar navbar-red"); 
+    document.getElementById("header").setAttribute("class", "header header-red"); 
+    document.getElementById("boxes").setAttribute("class", "boxes boxes-red"); 
+}
+function green() {
+    document.getElementById("body").setAttribute("class", "body-green"); 
+    document.getElementById("navbar").setAttribute("class", "navbar navbar-green"); 
+    document.getElementById("header").setAttribute("class", "header header-green"); 
+    document.getElementById("boxes").setAttribute("class", "boxes boxes-green"); 
+}
